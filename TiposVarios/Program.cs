5648fa7c37fc0266b48888a538f9b1c16a9fbb3f
Vitor <vitor.daniel@outlook.pt>
2023-03-01 23:25:53 +0000

    added chars and conversion to string for each one

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,13 +7,30 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            //variables for multiple ways to write "100"
             int Integer = 100;
             int Hexa = 0x64;
             int Binary = 0b0110_0100;
             
+            //write in console those ways in a line each
             Console.Write(Integer + "\n");
             Console.Write(Hexa + "\n");
-            Console.Write(Binary);
+            Console.Write(Binary + "\n");
+    
+            //variable for multiple chars
+            char SnowFlake = '\u2746';
+            char Skull = '\u2620';
+            char Communism = '\u262d';
+
+            //translate char into string
+            string SnowFlakeChar = Char.ToString(SnowFlake);
+            string SkullChar = Char.ToString(Skull);
+            string CommunismChar = Char.ToString(Communism);
+
+            //write in console these new strings
+            Console.Write(SnowFlake + "\n");
+            Console.Write(Skull + "\n");
+            Console.Write(Communism + "\n");
         }
     }
 }
