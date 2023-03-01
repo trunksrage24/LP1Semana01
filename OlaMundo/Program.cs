7e5b3d00d654a6f307e7d467ba8c4da5032c9d5a
Vitor <vitor.daniel@outlook.pt>
2023-03-01 19:54:50 +0000

    Add Bye World!

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -6,7 +6,7 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! \nBye World!");
         }
     }
 }
