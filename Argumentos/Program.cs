7c41dccb653fd8db299a0a247f6e6e8467667c16
Vitor <vitor.daniel@outlook.pt>
2023-03-01 22:39:28 +0000

    document Program.cs with a description of the file

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,6 +2,9 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// program to input multiple arguments and count them
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
