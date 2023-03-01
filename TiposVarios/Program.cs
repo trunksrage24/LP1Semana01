24928d036f3331daa6a270653cfa91e8cc296193
Vitor <vitor.daniel@outlook.pt>
2023-03-01 22:58:45 +0000

    program that prints 3 diferent ways to represent the number '100' to console

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Integer = 100;
+            int Hexa = 0x64;
+            int Binary = 0b0110_0100;
+            
+            Console.Write(Integer + "\n");
+            Console.Write(Hexa + "\n");
+            Console.Write(Binary);
+        }
+    }
+}
