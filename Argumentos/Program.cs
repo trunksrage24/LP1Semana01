32408121a406b11b28c70d4b7e4fff273c7d7de5
Vitor <vitor.daniel@outlook.pt>
2023-03-01 22:34:44 +0000

    add function to read arguments and print them while also counting them

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of Arguments: " + args.Length);  
+            Console.Write("Arguments Are: "); 
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
