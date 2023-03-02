e82b06bd4df8916014e399365000944f9eaa16d8
Vitor <vitor.daniel@outlook.pt>
2023-03-02 15:21:40 +0000

    add string variables and verbatim string variables and print on console

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string variables
+            string Sentence = "This is a sentence";
+            string VerbatimSentence = @"""This sentence is in quotation marks""";
+            string FilePath = @"C:\Users\vitor\OneDrive - Ensino Lusófona\Desktop\fcul\LP1\LP1Semana01\TiposVarios\Program.cs";
+
+            //write strings in console
+            Console.Write(Sentence + "\n");
+            Console.Write(VerbatimSentence + "\n");
+            Console.Write(FilePath + "\n");
+        }
+    }
+}
